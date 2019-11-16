ac84b5adf8b271760892b5ba3377d183f492da61
angiiie <angiecueto126@gmail.com>
2019-11-16 06:25:57 -0500

    codigos

---
+++ topo_PF.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=RemoteController,
+		                ip='127.0.0.1',
+                      protocol='tcp',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    s11 = net.addSwitch('s11', cls=OVSKernelSwitch)
+    s12 = net.addSwitch('s12', cls=OVSKernelSwitch)
+    s13 = net.addSwitch('s13', cls=OVSKernelSwitch)
+    
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(s11, s12,bw=8)
+    net.addLink(s12, s13,bw=8)
+    net.addLink(s11, h1,bw=8)
+    net.addLink(s12, h2,bw=8)
+    net.addLink(s13, h3,bw=8)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s11').start([c0])
+    net.get('s12').start([c0])
+    net.get('s13').start([c0])
+    
+
+    info( '*** Post configure switches and hosts\n')
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
